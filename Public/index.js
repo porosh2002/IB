c5d7304b378c818b7fdf71471ac57dafb99f2abf
porosh2002 <jamilkashem2002@gmail.com>
2020-08-12 16:04:10 -0400

    3.1.0

--- Public/index.js
+++ Public/index.js
@@ -18,7 +18,19 @@ var typed = new Typed(".type", {
     shuffle: 0
   });
   var swiper = new Swiper('.swiper-container', {
-    slidesPerView:4,
-    spaceBetween: 30,
+    breakpoints: {
+      320: {
+        slidesPerView: 2,
+        spaceBetween: 20
+      },
+      480: {
+        slidesPerView: 3,
+        spaceBetween: 30
+      },
+      640: {
+        slidesPerView: 4,
+        spaceBetween:30
+      }
+    },
     loop:true,
   });
